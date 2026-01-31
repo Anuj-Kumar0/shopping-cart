fd8ee7102e5f78639286414b5f4b2cc816c398b5
Anuj Kumar <anujkumar21052004@gmail.com>
2026-01-31 09:16:05 +0530

    Added Products

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,6 +1,30 @@
-function Shop() {
-    console.log("SHOP RENDERED");
-    return <p>Shop page</p>;
-}
+import useProducts from "../hooks/products";
+import styles from "./Shop.module.css";
 
-export default Shop;
+export default function Shop() {
+    const { products, loading, error } = useProducts();
+
+    if (loading)
+        return (
+            <div className={styles.loaderWrapper}>
+                <div className={styles.loader}></div>
+                <p>Loading products…</p>
+            </div>
+        );
+    if (error) return <p className={styles.status}>{error}</p>;
+
+    return (
+        <div className={styles.cards}>
+            {products.map(({ id, title, price, image }) => (
+                <div className={styles.card} key={id}>
+                    <div className={styles.imageWrapper}>
+                        <img src={image} alt={title} />
+                    </div>
+                    <h3>{title}</h3>
+                    <p className={styles.price}>${price}</p>
+                    <button className={styles.button}>Add to cart</button>
+                </div>
+            ))}
+        </div>
+    );
+}
