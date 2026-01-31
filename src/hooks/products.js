2b5544bf8af2ad550f2171223ae8ee0396f8dd7e
Anuj Kumar <anujkumar21052004@gmail.com>
2026-01-31 09:14:49 +0530

    Custom Hooks

---
+++ src/hooks/products.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+const api = "https://fakestoreapi.com/products";
+
+export default function useProducts() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchProducts() {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(api);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+
+      const productDetails = await response.json();
+      setProducts(productDetails);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return { products, error, loading };
+}
